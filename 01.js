5a80e231c534e17f4e875d21228b7f649d283ff6
lucalai <lucaslaiz1245@gmail.com>
2023-11-24 09:27:54 -0300

    01

--- 01.js
+++ 01.js
@@ -11,7 +11,13 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+      var nuevoArray = [];
+      array.forEach(elem => {
+        if(elem.typeof(Number)){
+          nuevoArray.push(elem);
+        }
+      });
+      return nuevoArray;
 }
 
 // No modifiques nada debajo de esta linea //
